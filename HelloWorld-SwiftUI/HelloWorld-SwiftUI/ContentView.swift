3753720ccecd9b7d325eb43384498abbd4ee4567
Ramprasad A <ramprasad.a@razorpay.com>
2021-06-17 13:27:40 +0530

    Vstack added

--- HelloWorld-SwiftUI/HelloWorld-SwiftUI/ContentView.swift
+++ HelloWorld-SwiftUI/HelloWorld-SwiftUI/ContentView.swift
@@ -9,8 +9,11 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world SwiftUI!")
-            .padding()
+        VStack {
+            Text("Hello, world SwiftUI!")
+                .padding()
+            Text("I am Ram")
+        }
     }
 }
 
