14cb04367402fad901a0bc3baecf0528a48662db
Ramprasad A <ramprasad.a@razorpay.com>
2021-06-17 12:18:28 +0530

    Initial Commit

---
+++ HelloWorld-SwiftUI/HelloWorld-SwiftUI/HelloWorld_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  HelloWorld_SwiftUIApp.swift
+//  HelloWorld-SwiftUI
+//
+//  Created by Ramprasad A on 17/06/21.
+//
+
+import SwiftUI
+
+@main
+struct HelloWorld_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
